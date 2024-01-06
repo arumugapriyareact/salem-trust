7a28d99b740418dedb684e823a3fe8916a18176f
Arumuga priya Iyyappan <arumugapriyaiyyappan@Arumugas-MacBook-Air.local>
2024-01-07 00:01:34 +0530

    netlify upgraded

--- routes/payment.js
+++ routes/payment.js
@@ -11,10 +11,12 @@ router.get("/check",async(req,res)=>{
     
 })
 router.post("/orders",async(req,res)=>{
+    console.log(req)
     try{
+      
         const instance= new Razorpay({
-            key_id: process.env.KEY_ID,
-            key_secret: process.env.KEY_SECRET
+            key_id: "rzp_test_GX4az0Kga9TjoI",
+            key_secret: "XwXATpfF8xcny6gYqCZWS4dR"
         })
 
         const options = {
@@ -22,7 +24,7 @@ router.post("/orders",async(req,res)=>{
             currency: "INR",
             receipt: crypto.randomBytes(10).toString("hex")
         }
-
+        
         instance.orders.create(options, (error, order)=>{
             if(error){
                 console.log(error);
@@ -46,24 +48,21 @@ router.get("/verify_get",async(req,res)=>{
 })
 router.post("/verify",async(req,res)=>{
     try{
-
         const {
             razorpay_order_id,
             razorpay_payment_id,
             razorpay_signature
         } = req.body
-        const sign = razorpay_order_id + "" + razorpay_payment_id
+        const sign = `${razorpay_order_id}|${razorpay_payment_id}`
         const expectedSign = crypto
-            .createHmac("sha256", process.env.KEY_SECRET)
+            .createHmac("sha256", "XwXATpfF8xcny6gYqCZWS4dR")
             .update(sign.toString())
             .digest("hex");
-
             if(razorpay_signature === expectedSign){
                 return res.status(200).json({message: "Payment Verified Successfully"})
             }else{
                 return res.status(400).json({message: "Invalid signature"})
             }
-
     }catch(error){
         console.log(error)
         res.status(500).json({message: "Internal Server Error!"})
